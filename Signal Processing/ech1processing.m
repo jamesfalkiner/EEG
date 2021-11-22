fdf18e1d9ef012360a7640a5f13b1f7ff940c8ec
jamesfalkiner <68999748+jamesfalkiner@users.noreply.github.com>
2021-11-22 11:54:53 +0100

    Added signal processing initial

---
+++ Signal Processing/ech1processing.m
@@ -0,0 +1,20 @@
+
+clear all
+close all
+clear variables
+T = readtable('test1data');
+%delete start up values
+T([1:200],:) = [];
+X=detrend_2d([T.EXGChannel1,T.Timestamp]);
+
+data = X(:,1);
+time = X(:,2);
+time2 = T.Timestamp_Formatted_;
+
+for k = 1:length(data)
+    data(k) = data(k)+time(k);
+end
+%plot(time2,data)
+%the x value needs to x + y
+
+plot(T.Timestamp,T.EXGChannel1)
---
+++ Signal Processing/test1.m
@@ -0,0 +1,19 @@
+clear all
+close all
+clear variables
+T = readtable('test1data');
+T([1],:) = [];
+% plot(T.Timestamp_Formatted_, T.EXGChannel0);
+hold on
+plot(T.Timestamp_Formatted_, T.EXGChannel1);
+plot(T.Timestamp_Formatted_, T.EXGChannel2);
+plot(T.Timestamp_Formatted_, T.EXGChannel3);
+plot(T.Timestamp_Formatted_, T.EXGChannel4);
+plot(T.Timestamp_Formatted_, T.EXGChannel5);
+plot(T.Timestamp_Formatted_, T.EXGChannel6);
+%plot(T.Timestamp_Formatted_, T.EXGChannel7);
+plot(T.Timestamp_Formatted_, T.AccelChannel0);
+plot(T.Timestamp_Formatted_, T.AccelChannel1);
+legend ("ech1","ech2","ech3","ech4","ech5","ech6","a0","a1")
+%plot(T.Timestamp_Formatted_, T.AccelChannel2);
+hold off
